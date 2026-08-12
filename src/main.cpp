0a4b83322d9b94d26433c765cff8e47a31670011
Leon <letiobi66@gmail.com>
2026-08-12 17:06:50 +0100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+int main()
+{
+	std::cout << "Data dir: " << DATA_DIR << std::endl;
+	std::ifstream f(std::string(DATA_DIR) + "/train.csv");
+	std::cout << (f.is_open() ? "found train.csv\n" : "train.csv not found\n");
+
+
+	return 0;
+}
